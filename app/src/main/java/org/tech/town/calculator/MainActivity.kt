fb3b17b7852d13376d25c01bad35da0d20471065
SooHwan <newpoo@naver.com>
2022-11-28 20:48:31 +0900

    feat: main 계산기 화면

--- app/src/main/java/org/tech/town/calculator/MainActivity.kt
+++ app/src/main/java/org/tech/town/calculator/MainActivity.kt
@@ -2,10 +2,19 @@ package org.tech.town.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun buttonClicked(v: View){
+
+    }
+
+    fun clearButtonClicked(view: View) {}
+    fun historyButtonClicked(view: View) {}
+    fun resultButtonClicked(view: View) {}
 }
