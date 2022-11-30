8dd5f8a99499a28f8889049a303be01b153a00cf
SooHwan <newpoo@naver.com>
2022-11-30 16:03:23 +0900

    feat: 숫자, 연산자 입력 기능

--- app/src/main/java/org/tech/town/calculator/MainActivity.kt
+++ app/src/main/java/org/tech/town/calculator/MainActivity.kt
@@ -2,19 +2,114 @@ package org.tech.town.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val expressionTextView: TextView by lazy {
+        findViewById<TextView>(R.id.expressionTextView)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById<TextView>(R.id.resultTextView)
+    }
+
+    //operator 입력 중
+    private var isOperator = false
+
+    //operator 하나만 사용하기 위해
+    private var hasOperator = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonClicked(v: View){
-
+    fun buttonClicked(v: View) {
+        when (v.id) {
+            R.id.zeroButton -> numberButtonClicked("0")
+            R.id.oneButton -> numberButtonClicked("1")
+            R.id.twoButton -> numberButtonClicked("2")
+            R.id.threeButton -> numberButtonClicked("3")
+            R.id.fourButton -> numberButtonClicked("4")
+            R.id.fiveButton -> numberButtonClicked("5")
+            R.id.sixButton -> numberButtonClicked("6")
+            R.id.sevenButton -> numberButtonClicked("7")
+            R.id.eightButton -> numberButtonClicked("8")
+            R.id.nineButton -> numberButtonClicked("9")
+            R.id.plusButton -> operatorButtonClicked("+")
+            R.id.minusButton -> operatorButtonClicked("-")
+            R.id.mutiButton -> operatorButtonClicked("*")
+            R.id.divideButton -> operatorButtonClicked("/")
+            R.id.moduloButton -> operatorButtonClicked("%")
+        }
     }
 
-    fun clearButtonClicked(view: View) {}
-    fun historyButtonClicked(view: View) {}
-    fun resultButtonClicked(view: View) {}
+    private fun numberButtonClicked(number: String) {
+
+        if (isOperator) {
+            expressionTextView.append(" ")
+        }
+
+        isOperator = false
+
+        val expressionText = expressionTextView.text.split(" ")
+
+        //????????????????
+        if (expressionText.isNotEmpty() && expressionText.last().length >= 15) {
+            Toast.makeText(this, "15자리 까지만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+            return
+        } else if (expressionText.last().isEmpty() && number == "0") {
+            Toast.makeText(this, "0은 제일 앞에 올 수 없습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        expressionTextView.append(number)
+        //TODO resultTextView 실시간으로  계산 결과 넣어야 하는 기능
+    }
+
+
+    private fun operatorButtonClicked(operator: String) {
+        if (expressionTextView.text.isEmpty()) {
+            return
+        }
+        when {
+            //operator 입력 중 다른 operator 입력 시 변경
+            isOperator -> {
+                val text = expressionTextView.text.toString()
+                expressionTextView.text = text.dropLast(1) + operator
+            }
+            //이미 operator 입력되어 있을 때
+            hasOperator -> {
+                Toast.makeText(this, "연산자는 한 번만 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                return
+            }
+            //isOperator, hasOperator 모두 false 인 경우 -> 연산자가 한번도 입력 되지 않은 경우
+            else -> {
+                expressionTextView.append(" $operator")
+            }
+        }
+        //operator 색상 변경
+        val ssb = SpannableStringBuilder(expressionTextView.text)
+        ssb.setSpan(
+            ForegroundColorSpan(getColor(R.color.green)),
+            expressionTextView.text.length - 1,
+            expressionTextView.text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        expressionTextView.text = ssb
+
+        isOperator = true
+        hasOperator = true
+    }
+
+    fun clearButtonClicked(v: View) {}
+
+    fun resultButtonClicked(v: View) {}
+
+    fun historyButtonClicked(v: View) {}
 }
